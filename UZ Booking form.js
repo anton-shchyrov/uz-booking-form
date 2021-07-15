98cec946d3720cec54dbd620819fb0e9fbc01f24
anton-shchyrov <anton-shchyrov@users.noreply.github.com>
2021-07-15 17:27:22 +0300

    Added check for checkbox exists

--- UZ Booking form.js
+++ UZ Booking form.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UZ Booking form
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://booking.uz.gov.ua/*
@@ -10,42 +10,89 @@
 
 (function() {
     'use strict';
+    console.log('load');
     const data = [{
-        firstName: "Name1",
-        lastName: "LastName1",
+        firstName: "Иван",
+        lastName: "Петров",
         services: {
             bedding: true,
-            tea: true,
+            tea: false,
             doubleTea: false
         },
     }, {
-        firstName: "Name2",
-        lastName: "LastName2",
+        firstName: "Иван",
+        lastName: "Петров",
         services: {
             bedding: true,
-            tea: true,
+            tea: false,
             doubleTea: false
         },
     }, {
-        firstName: "Name3",
-        lastName: "LastName3",
+        firstName: "Иван",
+        lastName: "Петров",
         services: {
             bedding: true,
-            tea: true,
+            tea: false,
+            doubleTea: false
+        },
+    }, {
+        firstName: "Иван",
+        lastName: "Петров",
+        services: {
+            bedding: true,
+            tea: false,
+            doubleTea: false
+        },
+    }, {
+        firstName: "Иван",
+        lastName: "Петров",
+        services: {
+            bedding: true,
+            tea: false,
+            doubleTea: false
+        },
+    }, {
+        firstName: "Иван",
+        lastName: "Петров",
+        services: {
+            bedding: true,
+            tea: false,
+            doubleTea: false
+        },
+    }, {
+        firstName: "Иван",
+        lastName: "Петров",
+        services: {
+            bedding: true,
+            tea: false,
+            doubleTea: false
+        },
+    }, {
+        firstName: "Иван",
+        lastName: "Петров",
+        services: {
+            bedding: true,
+            tea: false,
             doubleTea: false
         },
     }];
+
+    function setCheckBox(el, val) {
+        if (el)
+            el.checked = val;
+    }
+
     document.addEventListener("dblclick", function() {
+        console.log('dblclick');
         const forms = document.querySelectorAll(".place-item");
         console.log("Forms count: " + forms.length);
-        const count = Math.min(forms.length, data.length)l
-        for (let i = 0; i < count; i++) {
+        for (let i = 0; i < forms.length; i++) {
             const form = forms[i];
             form.querySelector("input[name='lastname']").value = data[i].lastName;
             form.querySelector("input[name='firstname']").value = data[i].firstName;
-            form.querySelector("input[name='bedding']").checked = data[i].services.bedding;
-            form.querySelector("input[name='services'][value='Н']").checked = data[i].services.tea;
-            form.querySelector("input[name='services'][value='М']").checked = data[i].services.doubleTea;
+            setCheckBox(form.querySelector("input[name='bedding']"), data[i].services.bedding);
+            setCheckBox(form.querySelector("input[name='services'][value='Н']"), data[i].services.tea);
+            setCheckBox(form.querySelector("input[name='services'][value='М']"), data[i].services.doubleTea);
         }
     });
 })();
